e963dd26c6ba56d00f2a53da5bfb64aa1b6bd553
zhangyifei <zhangyifei@wutongchian.com>
2021-09-26 14:20:18 +0800

    retry1

---
+++ src/A-2021-09-09/reTry1/demo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	num := randNum()
+
+	if num < 4 {
+		for i := 0; i < 500; i++ {
+			num = randNum()
+			if num < 4 {
+				fmt.Println("retry:", i, "--", num)
+				continue
+			} else {
+				fmt.Println("get u:", num)
+				return
+			}
+		}
+	} else {
+		fmt.Println("once")
+	}
+
+}
+
+func randNum() int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(5)
+}
